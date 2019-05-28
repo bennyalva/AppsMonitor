d321c9b438dfa6f3b60bfcda66812a92df91325a
Diego Cárcamo <diego.carcamo@axity.com>
2019-05-28 12:53:29 -0500

    validación notificaciones encendidas

--- service_python/alarm_analizer.py
+++ service_python/alarm_analizer.py
@@ -2,15 +2,16 @@ import mongo
 import notification_controller
 
 class AlarmAnalizer:
-    
+
     def __init__(self):
         self.mgo = mongo.MongoManager()
         self.notification = notification_controller.Notifications()
 
-    def analize(self, application, type, name, status_response, status):
-        event = { 'application': application,
+    def analize(self, point, type, name, status_response, status):
+        event = { 'application': point['application'],
                 'type': type,
                 'name': name }
+        print('status: ', status)
         if(status == False):
             similar_alarms = list(self.mgo.get_list('events', event, sort='datetime', sortDir='desc', limit=2))
             print('similar_alarms False', similar_alarms)
@@ -20,7 +21,8 @@ class AlarmAnalizer:
                     print('Mando mail se cayo')
                     print('Dest', self.get_dest_alarms(event))
                     print('Msg', self.create_alarm_message(False, event, status_response))
-                    self.notification.send_email(self.get_dest_alarms(event), self.create_alarm_message(False, event, status_response))
+                    if point['notifications'] == True:
+                        self.notification.send_email(self.get_dest_alarms(event), self.create_alarm_message(False, event, status_response))
         else:
             similar_alarms = list(self.mgo.get_list('events', event, sort='datetime', sortDir='desc', limit=2))
             print('similar_alarms True', similar_alarms)
@@ -30,14 +32,15 @@ class AlarmAnalizer:
                     print('Mando mail levanto')
                     print('Dest', self.get_dest_alarms(event))
                     print('Msg', self.create_alarm_message(True, event, status_response))
-                    self.notification.send_email(self.get_dest_alarms(event), self.create_alarm_message(True, event, status_response))
+                    if point['notifications'] == True:
+                        self.notification.send_email(self.get_dest_alarms(event), self.create_alarm_message(True, event, status_response))
         print('Termine')
-    
+
     def create_alarm_message(self, status, event, status_response):
         MAIL_CONTENT = ''
         if status == False:
             MAIL_CONTENT = ''' Buen día,
-            El sistema {} - {} se encuentra abajo, 
+            El sistema {} - {} se encuentra abajo,
             la respuesta obtenida fue: {}'''.format(event['application'], event['name'], status_response)
         else:
             MAIL_CONTENT = ''' Buen día,
--- service_python/crawler.py
+++ service_python/crawler.py
@@ -6,7 +6,7 @@ import alarm_analizer
 from datetime import datetime
 
 class Crawler:
-        
+
     def __init__(self):
         self.mgo = mongo.MongoManager()
         self.alm_analizer = alarm_analizer.AlarmAnalizer()
@@ -16,37 +16,37 @@ class Crawler:
 
     def get_points(self):
         return self.mgo.get_list('points', {})
-    
+
     def process(self, point):
         print('monitoring_application:', point['application'])
-        self.webscrapping(point['application'], point['sites'])
-        self.port_open(point['application'], point['services'])
-        self.database_status(point['application'], point['databases'])
+        self.webscrapping(point, point['sites'])
+        self.port_open(point, point['services'])
+        self.database_status(point, point['databases'])
         print('monitoring_application_finish:', point['application'])
-        
-    def webscrapping(self, application, sites):
+
+    def webscrapping(self, point, sites):
         for site in sites:
             print('webscrapping_to:', site['url'])
             result = webscrapping.invoke_site(site['url'])
-            self.alm_analizer.analize(application, 'sites', site['name'], result['msg'], result['status'])
+            self.alm_analizer.analize(point, 'sites', site['name'], result['msg'], result['status'])
             print('webscrapping_to_response:', result)
-            self.persist_event(application, 'sites', site['name'], result['msg'], result['status'])
-    
-    def port_open(self, application, services):
+            self.persist_event(point['application'], 'sites', site['name'], result['msg'], result['status'])
+
+    def port_open(self, point, services):
         for service in services:
             print('connecting_to_port:', '{}:{}'.format(service['ip'], service['port']))
             result = port_checker.check_port(service['ip'], service['port'])
-            self.alm_analizer.analize(application, 'services', service['name'], result, result)
+            self.alm_analizer.analize(point, 'services', service['name'], result, result)
             print('connecting_to_port_response:', result)
-            self.persist_event(application, 'services', service['name'], result, result)
-    
-    def database_status(self, application, databases):
+            self.persist_event(point['application'], 'services', service['name'], result, result)
+
+    def database_status(self, point, databases):
         for database in databases:
             print('verifying_dbconnection_to: ', 'ip:{}, db:{}'.format(database['ip'], database['database']))
             result = db_monitor.verify_connection('FreeTDS', database['ip'], database['database'], database['usr'], database['pwd'])
-            self.alm_analizer.analize(application, 'databases', database['name'], result['msg'], result['status'])
+            self.alm_analizer.analize(point, 'databases', database['name'], result['msg'], result['status'])
             print('verifying_dbconnection_to_response: ', result)
-            self.persist_event(application, 'databases', database['name'], result['msg'], result['status'])
+            self.persist_event(point['application'], 'databases', database['name'], result['msg'], result['status'])
 
     def persist_event(self, application, type, name, status_response, status):
         event = {
