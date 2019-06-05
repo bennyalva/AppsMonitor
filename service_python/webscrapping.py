d608ffba62a137c9c1550c7bf4d16deb5c9bc5f5
Diego Cárcamo <diego.carcamo@axity.com>
2019-06-05 11:33:42 -0500

    agregar excepcion generica a web check

--- service_python/webscrapping.py
+++ service_python/webscrapping.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 def invoke_site(url):
     try:
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
         response = requests.get(url, headers=headers)
         return {'status': response.status_code == 200, 'msg': 'Status Code: {}'.format(response.status_code)}
     except requests.exceptions.RequestException as ex:
@@ -16,6 +18,9 @@ def invoke_site(url):
         return {'status': False, 'msg': str(ex)}
     except requests.exceptions.RequestException as ex:
         return {'status': False, 'msg': str(ex)}
+    except Exception as ex:
+        return {'status': False, 'msg': str(ex)}
+
 
 def parse_response(html_response):
     soup = BeautifulSoup(html_response, 'html.parser')
