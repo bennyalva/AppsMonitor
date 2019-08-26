541025301d41ad2b6daee951d8a69da107efad46
bennyalva <bennytescha@gmail.com>
2019-08-26 18:57:33 -0500

    se agregan entorno virtual del proyecto

---
+++ service_python/local/alarm_analizer.py
@@ -0,0 +1,1 @@
+/home/benny/Documents/repositorios/codigo/angular/farmacias/AppsMonitor/service_python/alarm_analizer.py
---
+++ service_python/local/config.py
@@ -0,0 +1,1 @@
+/home/benny/Documents/repositorios/codigo/angular/farmacias/AppsMonitor/service_python/config.py
