d47dafc3d8b5a8877924c6bc0189b94ee5d60a6f
bennyalva <bennytescha@gmail.com>
2019-09-09 12:15:27 -0500

    se cambia version de python para correr el server docker service

--- service_python/config.py
+++ service_python/config.py
@@ -17,6 +17,7 @@ class ProductionConfig(Config):
     LISTEN_PORT = 3000
     LISTEN_ADDRESS = '0.0.0.0'
     MONGO_URL = 'mongodb'
+    #MONGO_URL = '10.11.0.123'
     EMAIL_HOST = 'smtp.office365.com'
     EMAIL_PORT = 0
     EMAIL_USER = ''
